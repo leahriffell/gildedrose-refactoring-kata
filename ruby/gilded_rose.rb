744969c08723bedafa68b3c2b5c9eb3cdcb7c574
Leah Riffell <leah.riffell@shopify.com>
2022-11-08 12:25:30 -0500

    Create ItemAdjuster class that adjusts item quality

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -28,14 +28,30 @@ class GildedRose
   end
 
   def expiry_rate(item)
-     if item.name == SULFURAS
-        return 0
-      else
-        return 1
-     end
+    if item.name == SULFURAS
+      return 0
+    else
+      return 1
+    end
   end
 
   def quality_adjustment(item)
+    ItemAdjuster.new(item).run
+  end
+end
+
+class ItemAdjuster
+  attr_reader :item
+
+  AGED_BRIE = "Aged Brie"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+
+  def initialize(item)
+    @item = item
+  end
+
+  def run
     case item.name
     when AGED_BRIE
       if item.sell_in < 1
@@ -43,25 +59,25 @@ class GildedRose
       else
         1
       end
-     when SULFURAS
-          0
-     when BACKSTAGE_PASSES
-        if item.sell_in >10
-          1
-        elsif item.sell_in > 5 && item.sell_in <= 10
-          2
-        elsif item.sell_in < 6 && item.sell_in > 0
-          3
-        elsif item.sell_in < 1
-          -1*item.quality
-        end
-      else
-        if item.sell_in < 1
-          -2
-        else
-          -1
-        end
+    when SULFURAS
+      0
+    when BACKSTAGE_PASSES
+      if item.sell_in >10
+        1
+      elsif item.sell_in > 5 && item.sell_in <= 10
+        2
+      elsif item.sell_in < 6 && item.sell_in > 0
+        3
+      elsif item.sell_in < 1
+        -1 * item.quality
       end
+    else
+      if item.sell_in < 1
+        -2
+      else
+        -1
+      end
+    end
   end
 end
 
